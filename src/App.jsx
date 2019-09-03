15bca1b1560f58b706aaf1b6e919e3996b3f1444
Matthew Alltop <Matthew.Alltop@salliemae.com>
2019-09-03 15:34:13 -0400

    Changed app to use SASS instead of regular CSS. Added my own eslint and package.json files. Removed boilerplate files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import './App.scss';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<Container fluid={true}>
+				<Row>
+					<Col xs={12}>
+						<div> Here's some text.</div>
+					</Col>
+				</Row>
+			</Container>
 		</div>
 	);
 }
